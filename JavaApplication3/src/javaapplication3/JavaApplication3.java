8943cd5d3955b5e91412ae1258bd7829e3f3300f
unknown <fms1541b@k3-426-10.edu.ugrasu>
2016-02-09 10:16:04 +0500

    Lab_3 Ulia

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * Парадова Юлия
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+   int[] nums=new int[20];//массив размер 
+   
+   for(int i=2;i<nums.length;i+=2){
+       System.out.print(" " + i + " "); 
+   }
+   System.out.println(""); 
+   for(int a=4;a<nums.length;a+=2){
+       System.out.println(" " + a);
+   }
+    
+    }
+    }
+
+
+   
+    
---
+++ JavaApplication4/src/javaapplication4/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author fms1541b
+ */
+public class NewClass {
+    public static void main(String[] args) {
+
+        int[] nums = new int[10];//массив размер 
+        for (int i = 0; i < nums.length; i ++) {
+            System.out.print(" " + i + " ");
+        }
+        }
+    }
+
+
+
